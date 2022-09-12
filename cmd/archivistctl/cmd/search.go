d06983106731bedc48d205fbd44bfb2cb41b7eda
Mikhail Swift <mikhail@testifysec.com>
2022-09-12 12:53:22 -0400

    chore: linting and copyright adjustments
    Signed-off-by: Mikhail Swift <mikhail@testifysec.com>

--- cmd/archivistctl/cmd/search.go
+++ cmd/archivistctl/cmd/search.go
@@ -1,3 +1,17 @@
+// Copyright 2022 The Archivist Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cmd
 
 import (
@@ -35,11 +49,11 @@ Digests are expected to be in the form algorithm:digest, for instance: sha256:45
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			conn, err := newConn(archivistUrl)
-			defer conn.Close()
 			if err != nil {
 				return nil
 			}
 
+			defer conn.Close()
 			algo, digest, err := validateDigestString(args[0])
 			if err != nil {
 				return err
--- cmd/archivistctl/cmd/store.go
+++ cmd/archivistctl/cmd/store.go
@@ -1,3 +1,17 @@
+// Copyright 2022 The Archivist Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cmd
 
 import (
@@ -21,11 +35,11 @@ var (
 		Args:         cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			conn, err := newConn(archivistUrl)
-			defer conn.Close()
 			if err != nil {
 				return err
 			}
 
+			defer conn.Close()
 			for _, filePath := range args {
 				if gitoid, err := storeAttestationByPath(cmd.Context(), archivist.NewCollectorClient(conn), filePath); err != nil {
 					return fmt.Errorf("failed to store %s: %w", filePath, err)
@@ -45,11 +59,11 @@ func init() {
 
 func storeAttestationByPath(ctx context.Context, client archivist.CollectorClient, path string) (string, error) {
 	file, err := os.Open(path)
-	defer file.Close()
 	if err != nil {
 		return "", err
 	}
 
+	defer file.Close()
 	return storeAttestation(ctx, client, file)
 }
 
