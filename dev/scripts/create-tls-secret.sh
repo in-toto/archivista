47bad29ac818dba9f309227539d4cf718a95c667
Cole <cole@testifysec.com>
2023-07-15 11:04:46 -0500

    chore: make skaffold config modular

--- dev/scripts/create-tls-secret.sh
+++ dev/scripts/create-tls-secret.sh
@@ -12,7 +12,5 @@ echo "Creating CA"
 CAROOT=$CAROOT mkcert -install -key-file "${CAROOT}"/key.pem -cert-file "${CAROOT}"/cert.pem testifysec.localhost *.testifysec.localhost localhost
 
 echo "Injecting CA into kubernetes"
-
-kubectl create namespace cert-manager || true
 kubectl create secret --dry-run=client -n cert-manager tls tls-secret --cert="${CAROOT}"/rootCA.pem --key="${CAROOT}"/rootCA-key.pem -o yaml | kubectl apply -f -
 kubectl -n kratos create configmap rootca --from-file="${CAROOT}/rootCA.pem" --dry-run=client -o yaml | kubectl apply -f -
