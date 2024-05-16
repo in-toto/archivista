9c790b0d8dd89b019d4cc807c06fa5181f77be57
chaosinthecrd <tom@tmlabs.co.uk>
2024-05-16 15:34:32 +0100

    adding ent schema changes for vex
    Signed-off-by: chaosinthecrd <tom@tmlabs.co.uk>

---
+++ ent/schema/vexstatement.go
@@ -0,0 +1,46 @@
+// Copyright 2022 The Archivista Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+)
+
+// VexStatement represents a VEX Statement
+type VexStatement struct {
+	ent.Schema
+}
+
+func (VexStatement) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id").NotEmpty(),
+	}
+}
+
+func (VexStatement) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("statements", Statement.Type),
+		edge.From("vexdocument", VexDocument.Type).Ref("statements").Unique().Required(),
+	}
+}
+
+func (VexStatement) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("id"),
+	}
+}
---
+++ internal/metadatastorage/vexstatement/parserstorer.go
@@ -0,0 +1,60 @@
+// Copyright 2023 The Archivista Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package attestationcollection
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/in-toto/archivista/ent"
+	"github.com/in-toto/archivista/internal/metadatastorage"
+	"github.com/openvex/go-vex/pkg/vex"
+)
+
+const (
+	Predicate = "https://openvex.dev/ns"
+)
+
+type ParsedVEX vex.VEX
+
+func Parse(data []byte) (metadatastorage.Storer, error) {
+	parsedVex := ParsedVEX{}
+	if err := json.Unmarshal(data, &parsedVex); err != nil {
+		return parsedVex, err
+	}
+
+	return parsedVex, nil
+}
+
+func (pv ParsedVEX) Store(ctx context.Context, tx *ent.Tx, stmtID int) error {
+	document, err := tx.VEXDocument.Create().
+		SetStatementID(stmtID).
+		SetID(pv.ID).
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, s := range pv.Statements {
+		if err := tx.VEXStatement.Create().
+			SetVEXDocumentID(document.ID).
+			SetVuln(s.Vulnerability.Name).
+			Exec(ctx); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
