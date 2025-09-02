ec8cf1696e30df4b57e56c290c9a4d05d55c2e72
Brad Beck <bradley.beck@gmail.com>
2025-09-02 08:43:52 -0500

    feat: AWS RDS IAM Authentication (#607)
    Support AWS RDS IAM Authentication.
    
    On-behalf-of: @Citi <bradley.beck@citi.com>
    
    Signed-off-by: Brad Beck <bradley.beck@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -16,13 +16,14 @@ FROM golang:1.25.0-alpine@sha256:f18a072054848d87a8077455f0ac8a25886f2397f88bfdd
 WORKDIR /src
 RUN apk update && apk add --no-cache file git curl
 RUN curl -sSf https://atlasgo.sh | sh
-ENV GOMODCACHE /root/.cache/gocache
+ENV GOMODCACHE=/root/.cache/gocache
 RUN --mount=target=. --mount=target=/root/.cache,type=cache \
-    CGO_ENABLED=0 go build -o /out/archivista -ldflags '-s -d -w' ./cmd/archivista; \
+    CGO_ENABLED=0 go build -o /out/ -ldflags '-s -d -w' ./cmd/...; \
     file /out/archivista | grep "statically linked"
 
 FROM alpine:3.22.1@sha256:4bcff63911fcb4448bd4fdacec207030997caf25e9bea4045fa6c8c44de311d1
 COPY --from=build /out/archivista /bin/archivista
+COPY --from=build /out/archivistactl /bin/archivistactl
 COPY --from=build /usr/local/bin/atlas /bin/atlas
 ADD entrypoint.sh /bin/entrypoint.sh
 ADD ent/migrate/migrations /archivista/migrations
--- entrypoint.sh
+++ entrypoint.sh
@@ -24,17 +24,25 @@ else
     SQL_TYPE=$(echo "$ARCHIVISTA_SQL_STORE_BACKEND" | tr '[:lower:]' '[:upper:]')
   fi
   case $SQL_TYPE in
-  MYSQL)
+  MYSQL*)
     if [[ -z $ARCHIVISTA_SQL_STORE_CONNECTION_STRING ]]; then
       ARCHIVISTA_SQL_STORE_CONNECTION_STRING="root:example@db/testify"
     fi
     echo "Running migrations for MySQL"
-    atlas migrate apply --dir "file:///archivista/migrations/mysql" --url "mysql://$ARCHIVISTA_SQL_STORE_CONNECTION_STRING"
+    DB_URL="mysql://${ARCHIVISTA_SQL_STORE_CONNECTION_STRING}"
+    if [[ "$SQL_TYPE" == *_RDS_IAM ]]; then
+      DB_URL=$(/bin/archivistactl iam "${SQL_TYPE}" "${DB_URL}")
+    fi
+    atlas migrate apply --dir "file:///archivista/migrations/mysql" --url "${DB_URL}"
     atlas_rc=$?
     ;;
-  PSQL)
+  PSQL*)
     echo "Running migrations for Postgres"
-    atlas migrate apply --dir "file:///archivista/migrations/pgsql" --url "$ARCHIVISTA_SQL_STORE_CONNECTION_STRING"
+    DB_URL="${ARCHIVISTA_SQL_STORE_CONNECTION_STRING}"
+    if [[ "$SQL_TYPE" == *_RDS_IAM ]]; then
+      DB_URL=$(/bin/archivistactl iam "${SQL_TYPE}" "${DB_URL}")
+    fi
+    atlas migrate apply --dir "file:///archivista/migrations/pgsql" --url "${DB_URL}"
     atlas_rc=$?
     ;;
   *)
